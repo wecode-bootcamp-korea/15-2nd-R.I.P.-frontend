eb5720ca18e7a776761833c3220eae833507de04
123 <123>
2021-01-06 12:51:39 +0900

    Add : nav, sign

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-<<<<<<< HEAD
 
 const { Kakao } = window;
 
-=======
->>>>>>> 3ea23ac... Modify: 코드 리팩토링 완료
 ReactDOM.render(<Routes />, document.getElementById("root"));
